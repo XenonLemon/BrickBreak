2ac2d98c08a795dd0784feeb18f4601a23895188
XenonLemon <pd922503@gmail.com>
2025-12-09 19:28:43 +0000

    Commit 1 12/9/25

--- src/main/java/com/BrickBreak/ Paddle.java
+++ src/main/java/com/BrickBreak/ Paddle.java
@@ -1,3 +1,7 @@
+//Peter Daniel
+//12/9/25
+//This class will be used to create the paddel that will be used by the player in the Brick Break game.
+
 package com.BrickBreak;
 
 import java.awt.Graphics;
@@ -8,14 +12,76 @@ public class Paddle {
 
 	//your code here!
 
-
-	
 	//don't forget you need instance variables:
+	private int x;
+	private int y;
+	private int width;
+	private int height;
+	private int xVelocity;
+
 	
 	
 	//constructor(s):
+	public Paddle(int x, int y, int width, int height){
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		this.xVelocity = 6;
+	}
 	
+	//Getters and Setters:
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	public int getVelocity(){
+		return xVelocity;
+	}
+
+	public void setVelocity(int xVelocity){
+		this.xVelocity = xVelocity;
+	}
+
+	public void addVelocity(int addVelocity){
+		this.xVelocity += addVelocity;
+	}
+
+
+
 	//methods:
+	public void draw(Graphics g){
+		g.setColor(Color.white);
+		g.fillRect(x, y, width, height);
+	}
 	
 	
 }
--- src/main/java/com/BrickBreak/Ball.java
+++ src/main/java/com/BrickBreak/Ball.java
@@ -1,3 +1,7 @@
+//Peter Daniel
+//12/9/25
+//This class will be used to create the ball that will be used in the Brick Break game.
+
 package com.BrickBreak;
 import java.awt.*;
 
@@ -5,11 +9,27 @@ public class Ball {
 	//your code here!
 	
 	//don't forget you need instance variables:
+	private int x;
+	private int y;
+	private int size;
+	private int yVelocity;
+	private int xVelocity;
 	
 	
 	
 	//constructor(s):
+	public Ball(int x, int y, int size){
+		this.x = x;
+		this.y = y;
+		this.size = size;
+		xVelocity = 5;
+		yVelocity = 5;
+	}
 	
 	//methods:
+	public void draw(Graphics g){
+		g.setColor(Color.cyan);
+		g.fillOval(x, y, size, size);
+	}
 
 }
